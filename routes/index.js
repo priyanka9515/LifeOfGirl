196aa1b15b0fd89849234f0a8ad18060e15dc9dc
BigDaddy-Zephyr <anujduggal61@gmail.com>
2019-10-22 21:45:50 +0530

    Added signin, not complete

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,50 @@ router.get('/ask', function(req, res, next) {
 });
 
 
+router.get('/submit', function(req, res, next) {
+  res.render('submit', { title: 'Submit' });
+});
+
+
+
+router.post('/addpost', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+    // Set our collection
+   var userCollection = db.collection('user');
+    // Get our form values. These rely on the "name" attributes
+    var title = req.body.title;
+    var desc = req.body.desc;
+    var link = req.body.link;
+    
+    var currentTime = new Date();
+
+    var time = currentTime.getTime();
+    	
+    	id 	 = 
+  
+
+    // Submit to the DB
+    userCollection.insert({
+        "id" : id,
+        "password" :password}, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect('/');
+        }
+    });
+
+});
+
+
+
+
+
 
 router.post('/adduser', function(req, res) {
 
