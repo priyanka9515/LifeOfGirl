c1a845e41ee393fa3d20afae29fe1e4a599d2fbd
BigDaddy-Zephyr <anujduggal61@gmail.com>
2019-10-23 12:20:48 +0530

    Sign Up Sign in is working, bugs fixed

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,10 @@ router.get('/submit', function(req, res, next) {
 });
 
 
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'SignIn' });
+});
+
 
 router.post('/addpost', function(req, res) {
 
@@ -42,13 +46,16 @@ router.post('/addpost', function(req, res) {
 
     var time = currentTime.getTime();
     	
-    	id 	 = 
+    	id 	 = window.localStorage.getItem('user');
   
 
     // Submit to the DB
     userCollection.insert({
-        "id" : id,
-        "password" :password}, function (err, doc) {
+        "title" : title,
+        "desc" :desc,
+        "link"	:link,
+        "time"	:time,
+        "id"	:id}, function (err, doc) {
         if (err) {
             // If it failed, return error
             res.send("There was a problem adding the information to the database.");
@@ -60,6 +67,30 @@ router.post('/addpost', function(req, res) {
     });
 
 });
+
+
+
+router.post('/login', function(req,res){
+
+ var db = req.db;
+    // Set our collection
+   var userCollection = db.collection('user');
+    // Get our form values. These rely on the "name" attributes
+    var id = req.body.name;
+    var password = req.body.password;
+    var query = {"id": id, "password": password}
+     db.collection('user').find(query, function(err, user){
+      if(err) throw new Error(err);
+      if(!user) 
+        console.log('Not found');
+      else 
+        console.log('Found!');
+    	res.redirect('/');
+  })
+});
+
+
+
 
 
 
@@ -76,8 +107,7 @@ router.post('/adduser', function(req, res) {
     var id = req.body.name;
     var password = req.body.password;
 
- 
-  
+
 
     // Submit to the DB
     userCollection.insert({
