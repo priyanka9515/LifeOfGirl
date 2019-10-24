c5ca66e86485089b16785a02f8fa5dae8d4f8f83
BigDaddy-Zephyr <anujduggal61@gmail.com>
2019-10-24 15:37:43 +0530

    added script.js

---
+++ views/script.js
@@ -0,0 +1,17 @@
+
+
+console.log(hi);
+
+
+
+    var name,password;
+    $("#submit").click(function(){
+        name=$("#name").val();
+        password=$("#password").val();
+        console.log("$$$$$$$$$$$$$", name, password)
+        $.post("/login", {name: name, password: password} ,function(data){
+            console.log("AJAx");
+        });
+        console.log("@@@@@@@@@@@@@@@@@");
+        localStorage.setItem('user',name);
+    });
